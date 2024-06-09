ffaef6ebd5bcd03d29dcdea30e39dc2e9d24815a
Marco Herrera <marcohg0312@gmail.com>
2024-06-08 23:50:07 -0600

    fixes

--- Code/backend/src/interfaces/IProductDAO.ts
+++ Code/backend/src/interfaces/IProductDAO.ts
@@ -1,10 +1,9 @@
-
-import { Product as ProductModel } from "../../models/Product";
+import { Product as ProductModel } from "../models/Product";
 export interface IProductDAO {
-    getProduct(productId: string): Promise<any>;
-    getProducts(): Promise<any>;
-    registerProduct(productToRegister: ProductModel): Promise<any>;
-    updateProduct(productToUpdate: ProductModel): Promise<any>;
-    updateProductUnits(productId: string, units: number): Promise<any>;
-    deleteProduct(productId: string): Promise<any>;
+  getProduct(productId: string): Promise<any>;
+  getProducts(): Promise<any>;
+  registerProduct(productToRegister: ProductModel): Promise<any>;
+  updateProduct(productToUpdate: ProductModel): Promise<any>;
+  updateProductUnits(productId: string, units: number): Promise<any>;
+  deleteProduct(productId: string): Promise<any>;
 }
--- Code/backend/src/interfaces/IPublicationDAO.ts
+++ Code/backend/src/interfaces/IPublicationDAO.ts
@@ -1,11 +1,11 @@
-import { Publication as PublicationModel } from "../../models/Publication";
+import { Publication as PublicationModel } from "../models/Publication";
 
 export interface IPublicationDAO {
-  getPublication(publicationId: string): Promise<any>; 
-  getPublications(): Promise<any[]>; 
-  getPublicationsByCategory(categoryId: string): Promise<any[]>; 
-  getPublicationsByTags(tags: string[]): Promise<any[]>; 
-  registerPublication(publicationToRegister: PublicationModel): Promise<any>; 
+  getPublication(publicationId: string): Promise<any>;
+  getPublications(): Promise<any[]>;
+  getPublicationsByCategory(categoryId: string): Promise<any[]>;
+  getPublicationsByTags(tags: string[]): Promise<any[]>;
+  registerPublication(publicationToRegister: PublicationModel): Promise<any>;
   updatePublication(publicationToUpdate: PublicationModel): Promise<any>;
-  deletePublication(publicationId: string): Promise<any>; 
+  deletePublication(publicationId: string): Promise<any>;
 }
